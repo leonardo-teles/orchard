6dc870e8f5861f5167282c4dd4c9109a7c1a253d
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-14 19:31:25 -0300

    Creating Account Service

---
+++ backend/src/main/java/com/orchard/service/AccountService.java
@@ -0,0 +1,33 @@
+package com.orchard.service;
+
+import java.util.List;
+
+import com.orchard.model.Role;
+import com.orchard.model.User;
+
+public interface AccountService {
+
+	public void saveUser(User user);
+	
+	public User findByUsername(String username);
+	
+	public User findByEmail(String email);
+	
+	public List<User> userList();
+	
+	public Role findUserRoleByName(String role);
+	
+	public Role saveRole(Role role);
+	
+	public void updateUser(User user);
+	
+	public User findById(Long id);
+	
+	public void deleteUser(User user);
+	
+	public void resetPassword(User user);
+	
+	public List<User> getUserListByUsername(String username);
+	
+	public void simpleSave(User user);
+}
