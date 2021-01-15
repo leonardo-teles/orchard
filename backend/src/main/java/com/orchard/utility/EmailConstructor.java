3ebb9f6fd62b3c018caead5b35f0165eeec3a575
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-14 21:22:50 -0300

    e-Mail Utility

---
+++ backend/src/main/java/com/orchard/utility/EmailConstructor.java
@@ -0,0 +1,45 @@
+package com.orchard.utility;
+
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import com.orchard.model.User;
+
+public class EmailConstructor {
+
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private TemplateEngine template;
+	
+	public MimeMessagePreparator newUserEmail(User user, String password) {
+		Context context = new Context();
+		context.setVariable("user", user);
+		context.setVariable("password", password);
+		
+		String html = template.process("email/newUser", context);
+		
+		MimeMessagePreparator messagePreparator = new MimeMessagePreparator() {
+			
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, "UTF-8"); 
+				helper.setPriority(1);
+				helper.setTo(user.getEmail());
+				helper.setText(html, true);
+				helper.setSubject("Welcome to Orchart");
+				helper.setFrom(new InternetAddress(env.getProperty("support.email")));
+			}
+		};
+		
+		return messagePreparator;
+	}
+}
