10648ce90ed01af11f06bf0313ad2cccb64adccd
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-31 01:42:29 -0300

    Creating Server Constants

---
+++ frontend/src/app/constant/server.ts
@@ -0,0 +1,11 @@
+export class Server {
+
+  //DEV ENVIRONMENTS
+  public host: string = 'http://localhost:8080';
+
+  public client: string = 'http://localhost:4200';
+
+  public userPicture: string = `${this.host}/image/user`;
+
+  public postPicture: string = `${this.host}/image/post`;
+}
