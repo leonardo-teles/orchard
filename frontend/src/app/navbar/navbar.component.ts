3b019cc79b90b5598ef894f6b40073a88cd8e618
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-04 16:04:12 -0300

    Home Component

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -104,9 +104,7 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
 
   onNewPost(post: Post): void {
-    const element: HTMLElement = document.getElementById(
-      'dismissOnSubmitPost'
-    ) as HTMLElement;
+    const element: HTMLElement = document.getElementById('dismissOnSubmitPost') as HTMLElement;
     element.click();
     this.loadingService.isLoading.next(true);
     this.subscriptions.push(
