547a3dcb360779e4d9f2e203d73107342ff53292
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-03 23:43:50 -0300

    Registering Services

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+// components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -9,7 +15,23 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
-import { Routes } from '@angular/router';
+
+// services
+import { AlertService } from './service/alert.service';
+import { AccountService } from './service/account.service';
+import { LoadingService } from './service/loading.service';
+import { PostService } from './service/post.service';
+import { PostresolverService } from './service/postresolver.service';
+
+// guards
+import { AuthenticationGuard } from './guard/authentication.guard';
+
+// interceptors
+import { CacheInterceptor } from './interceptor/cache.interceptor';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
+
+// external modules
+import { NgxLoadingModule } from 'ngx-loading';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent},
@@ -34,9 +56,24 @@ const appRoutes: Routes = [
     PostDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    NgxLoadingModule.forRoot({})
   ],
-  providers: [],
+  providers: [
+    AccountService,
+    LoadingService,
+    PostService,
+    AlertService,
+    PostresolverService,
+    AuthenticationGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
