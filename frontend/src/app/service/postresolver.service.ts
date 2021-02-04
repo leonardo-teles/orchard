a8bedddfc22cf585bd83aa7c5d8ed277c7a785ed
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-03 22:55:46 -0300

    Post Resolver

---
+++ frontend/src/app/service/postresolver.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Post } from '../model/post';
+import { PostService } from './post.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostresolverService implements Resolve<Post>{
+
+  constructor(private postService: PostService) { }
+
+  resolve(route: ActivatedRouteSnapshot): Observable<Post> {
+    const postId: number = route.params['postId'];
+
+    return this.postService.getOnePostById(postId);
+  }
+}
